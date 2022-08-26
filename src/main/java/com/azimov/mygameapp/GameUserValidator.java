3e86b98eda08b4ca6707d1fb87331f70229fe8ca
Oleg Azimov <oleg_azimov_00@mail.ru>
2022-08-26 16:59:29 +0400

    Changed configuration

--- src/main/java/com/azimov/mygameapp/GameUserValidator.java
+++ src/main/java/com/azimov/mygameapp/GameUserValidator.java
@@ -2,20 +2,21 @@ package com.azimov.mygameapp;
 
 import com.azimov.mygameapp.models.GameUser;
 import com.azimov.mygameapp.services.GameUsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 @Component
 public class GameUserValidator implements Validator {
     private final GameUsersService gameUsersService;
-
+    @Autowired
     public GameUserValidator(GameUsersService gameUsersService) {
         this.gameUsersService = gameUsersService;
     }
 
     @Override
     public boolean supports(Class<?> clazz) {
-        return GameUser.class.equals(clazz);
+        return false;
     }
 
     @Override
