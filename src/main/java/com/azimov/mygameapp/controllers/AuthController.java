484996a198c72a693214325785316604710160d1
Oleg Azimov <oleg_azimov_00@mail.ru>
2022-09-06 14:21:49 +0400

    Changed configuration

--- src/main/java/com/azimov/mygameapp/controllers/AuthController.java
+++ src/main/java/com/azimov/mygameapp/controllers/AuthController.java
@@ -41,7 +41,7 @@ public class AuthController {
         if(bindingResult.hasErrors()){
             System.out.println("BINDING RESULT ERROR");
             model.addAttribute("gameUser", gameUser);
-            return "/auth/registration";}
+            return "/auth/registration.html";}
         registrationService.register(gameUser);
 
         return "redirect:/auth/login";
