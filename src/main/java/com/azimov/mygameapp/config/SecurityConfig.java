c75c010236b4bd0447411e7ccd05a4e656a4f8cb
L Sklonny <l.sklonnaya@ritg.ru>
2023-02-10 20:43:29 +0400

    добавлены скрипты создания таблиц, скрипты создания столбцов (liquibase), наведен порядок в css

--- src/main/java/com/azimov/mygameapp/config/SecurityConfig.java
+++ src/main/java/com/azimov/mygameapp/config/SecurityConfig.java
@@ -3,13 +3,15 @@ package com.azimov.mygameapp.config;
 import com.azimov.mygameapp.services.GameUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final GameUserDetailsService gameUserDetailsService;
@@ -37,10 +39,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(gameUserDetailsService)
-                .passwordEncoder(getPasswordEncoder());
+        auth.authenticationProvider(authProvider());
     }
 
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(gameUserDetailsService);
+        authProvider.setPasswordEncoder(getPasswordEncoder());
+        return authProvider;
+    }
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
