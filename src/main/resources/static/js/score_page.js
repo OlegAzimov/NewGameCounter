5eccae08e1194291c1bb30b40d53753a589137b2
L Sklonny <l.sklonnaya@ritg.ru>
2023-02-27 19:50:51 +0400

    исправление неймингов liquibase

--- src/main/resources/static/js/score_page.js
+++ src/main/resources/static/js/score_page.js
@@ -1,14 +1,64 @@
-$("#checkbox-container :checkbox").on("change", function(){
-});
+const sidebar = document.querySelector('.sidebar');
+sidebar.querySelector('.blocker').onclick = hide;
+
+function show() { // swipe right
+    sidebar.classList.add('visible');
+    document.body.style.overflow = 'hidden';
+}
+
+function hide() { // by blocker click, swipe left, or url change
+    sidebar.classList.remove('visible');
+    document.body.style.overflow = '';
+}
+
+function toggle() {
+    sidebar.classList.contains('visible') ? hide() : show();
+}
+
+window.toggle = toggle;
+
 var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {};
 var $checkboxes = $("#checkbox-container :checkbox");
-
-$checkboxes.on("change", function(){
-    $checkboxes.each(function(){
+var $barCheckboxes = $("#bar-checkbox-container :checkbox");
+$checkboxes.on("change", function () {
+    $checkboxes.each(function () {
         checkboxValues[this.id] = this.checked;
     });
     localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
 });
-$.each(checkboxValues, function(key, value) {
-    $("#" + key).prop('checked', value);
+$barCheckboxes.on("change", function () {
+    $barCheckboxes.each(function () {
+        checkboxValues[this.id] = this.checked;
+    });
+    localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
 });
+$.each(checkboxValues, function (key, value) {
+    $("#" + key).prop('checked', value);
+    $("#" + "bar" + key).prop('checked', value);
+})
+
+function allUncheck() {
+    document.getElementsByName('gameList').forEach(el => {
+        el.checked = false
+    });
+    $checkboxes.each(function () {
+        checkboxValues[this.id] = false;
+    });
+    $barCheckboxes.each(function () {
+        checkboxValues[this.id] = false;
+    });
+    localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+}
+
+function allCheck() {
+    document.getElementsByName('gameList').forEach(el => {
+        el.checked = true
+    })
+    $checkboxes.each(function () {
+        checkboxValues[this.id] = true;
+    });
+    $barCheckboxes.each(function () {
+        checkboxValues[this.id] = true;
+    });
+    localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+}
